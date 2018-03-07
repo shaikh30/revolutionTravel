aae792c3cdcb714b5c1bed2e71ef43406667bc27
sadaf30 <sadaf.shaikh@hotmail.com>
2018-03-07 15:03:47 -0500

    done

---
+++ snippets/spam-proof-email.js
@@ -0,0 +1,18 @@
+<script>
+	var st1 = "<a ";
+	var st2 = "hre";
+	var st3 = "f=mai";
+	var st4 = "lto:";
+
+	var e1 = "your-name";
+	var e2 = "@";
+	var e3 = "your-domain";
+	var e4 = ".";
+	var e5 = "com";
+	var eDone = e1+e2+e3+e4+e5;
+
+	var end1 = "</";
+	var end2 = "a>";
+
+	document.write(st1+st2+st3+st4+eDone+">"+eDone+end1+end2);
+</script>
